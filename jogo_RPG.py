9f97233027b0f44f9d56c4a6408066675e5c13bb
IssamuHuti <lucas.issamum@gmail.com>
2025-05-13 22:05:18 -0300

    13/05/25

--- jogo_RPG.py
+++ jogo_RPG.py
@@ -10,6 +10,7 @@ criar um jogo estilo rpg(utilizar POO)
 '''
 
 import os
+import random
 
 def limpar():
     os.system('cls')
@@ -30,8 +31,8 @@ class Atributos:
             ATQ = self.ATQ * 1.5 * self.tempo
             DEF = self.DEF * 1.5 * self.tempo
             VEL = self.VEL * 1.5 * self.tempo
-            EVA = self.EVA * 1.5 * self.tempo
-            return [HP, ATQ, DEF, VEL, EVA]
+            EVA = self.EVA * 1.3 * self.tempo
+            return {'HP' :HP, 'ATQ':ATQ, 'DEF':DEF, 'VEL':VEL, 'EVA':EVA}
         
         if self.profissao == 'tanque':
             HP = self.HP * 2 * self.tempo
@@ -39,25 +40,77 @@ class Atributos:
             DEF = self.DEF * 2 * self.tempo
             VEL = self.VEL * 1.2 * self.tempo
             EVA = self.EVA * 1.2 * self.tempo
-            return [HP, ATQ, DEF, VEL, EVA]
+            return {'HP' :HP, 'ATQ':ATQ, 'DEF':DEF, 'VEL':VEL, 'EVA':EVA}
         
         if self.profissao == 'arqueiro':
             HP = self.HP * 1.2 * self.tempo
             ATQ = self.ATQ * 2.5 * self.tempo
             DEF = self.DEF * 1.1 * self.tempo
             VEL = self.VEL * 2 * self.tempo
-            EVA = self.EVA * 2 * self.tempo
-            return [HP, ATQ, DEF, VEL, EVA]
+            EVA = self.EVA * 1.5 * self.tempo
+            return {'HP' :HP, 'ATQ':ATQ, 'DEF':DEF, 'VEL':VEL, 'EVA':EVA}
             
 rodada = 1
+profissao = 'Escolha a profissão:\n1 - Guerreiro\n2 - Tanque\n3 - Arqueiro\n=> '
+
 while True:
     p1 = Atributos('guerreiro', rodada)
     p2 = Atributos('tanque', rodada)
     p3 = Atributos('arqueiro', rodada)
 
-    print(p1.atributoDistribuicao())
-    print(p2.atributoDistribuicao())
-    print(p3.atributoDistribuicao())
+    while True:
+        limpar()
+        atributosRodada = int(input(profissao))
+        if atributosRodada in [1, 2, 3]:
+            break
+        else:
+            print('Informe uma opção válida')
+            input()
+
+    limpar()
+
+    if atributosRodada == 1:
+        campeao = p1.atributoDistribuicao()
+        print('GUERREIRO')
+        for atributo, valor in p1.atributoDistribuicao().items():
+            print(f'{atributo}: {valor}')
+        print()
+
+    elif atributosRodada == 2:
+        campeao = p2.atributoDistribuicao()
+        print('TANQUE')
+        for atributo, valor in p2.atributoDistribuicao().items():
+            print(f'{atributo}: {valor}')
+        print()
+
+    elif atributosRodada == 3:
+        campeao = p3.atributoDistribuicao()
+        print('ARQUEIRO')
+        for atributo, valor in p3.atributoDistribuicao().items():
+            print(f'{atributo}: {valor}')
+        print()
+
+    oponenteEscolha = random.choice([1, 2, 3])
+    if oponenteEscolha == 1:
+        oponente = p1.atributoDistribuicao()
+        print('GUERREIRO')
+        for atributo, valor in p1.atributoDistribuicao().items():
+            print(f'{atributo}: {valor}')
+        print()
+
+    elif oponenteEscolha == 2:
+        oponente = p2.atributoDistribuicao()
+        print('TANQUE')
+        for atributo, valor in p2.atributoDistribuicao().items():
+            print(f'{atributo}: {valor}')
+        print()
+
+    elif oponenteEscolha == 3:
+        oponente = p3.atributoDistribuicao()
+        print('ARQUEIRO')
+        for atributo, valor in p3.atributoDistribuicao().items():
+            print(f'{atributo}: {valor}')
+        print()
 
     rodada += 1
     input()

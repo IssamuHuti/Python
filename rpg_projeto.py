f3ecd29ab391c074ce6e2852c765854fa45a668f
IssamuHuti <lucas.issamum@gmail.com>
2025-06-04 22:40:06 -0300

    04/06/25

--- rpg_projeto.py
+++ rpg_projeto.py
@@ -8,6 +8,7 @@ def limpar():
 class AtributosHeroi:
     def __init__(self):
         self.level = 1
+        self.xp = 0
         self.hp = 10
         self.mp = 10
         self.ataque = 10
@@ -17,40 +18,50 @@ class AtributosHeroi:
         self.vel = 10
         self.sorte = 10
     
-    def distribuicaoPontos(self):
+    # problemas para informar os pontos de atributos para distribuição
+    def aumentoNivel(self):
+        self.level += 1
+
         pontosDistribuir = 5
 
         print('Distribua os pontos (pressione ESC para sair):')
-        print('HP (q)\nMP (w)\nATQ.F (e)\nATQ.M (r)\nDEF.F (a)\nDEF.M (s)\nVEL (d)\nSorte (f)')
 
+        # não está diminuindo o valor dos pontosDistribuir, não está informando a quantidade de pontos a distribuir restante
         while pontosDistribuir > 0:
-            if keyboard.is_pressed('q'):
-                self.hp += 1
-                pontosDistribuir -= 1
-            elif keyboard.is_pressed('w'):
-                self.mp += 1
-                pontosDistribuir -= 1
-            elif keyboard.is_pressed('e'):
-                self.ataque += 1
-                pontosDistribuir -= 1
-            elif keyboard.is_pressed('r'):
-                self.atqMag += 1
-                pontosDistribuir -= 1
-            elif keyboard.is_pressed('a'):
-                self.defesa += 1
-                pontosDistribuir -= 1
-            elif keyboard.is_pressed('s'):
-                self.defMagia += 1
-                pontosDistribuir -= 1
-            elif keyboard.is_pressed('d'):
-                self.vel += 1
-                pontosDistribuir -= 1
-            elif keyboard.is_pressed('f'):
-                self.sorte += 1
-                pontosDistribuir -= 1
-            elif keyboard.is_pressed('esc'):
-                print(f'Pontos a distribuir restantes: {pontosDistribuir}')
-                break
+            print(f'Pontos a distribuir: {pontosDistribuir}')
+            print('HP    (q)\nMP    (w)\nATQ.F (e)\nATQ.M (r)\nDEF.F (a)\nDEF.M (s)\nVEL   (d)\nSorte (f)')
+
+            evento = keyboard.read_event() # cria uma variavel que recebe a informação de que anteriormente já foi feita o recebimento da insersão da tecla
+            if evento.event_type == keyboard.KEY_DOWN: # cria um evento para que não permita acontecer erro de reler a tecla do loop anterior
+                teclaAtribuicao = keyboard.read_key()
+
+                if teclaAtribuicao == 'q':
+                    self.hp += 1
+                    pontosDistribuir -= 1
+                elif teclaAtribuicao == 'w':
+                    self.mp += 1
+                    pontosDistribuir -= 1
+                elif teclaAtribuicao == 'e':
+                    self.ataque += 1
+                    pontosDistribuir -= 1
+                elif teclaAtribuicao == 'r':
+                    self.atqMag += 1
+                    pontosDistribuir -= 1
+                elif teclaAtribuicao == 'a':
+                    self.defesa += 1
+                    pontosDistribuir -= 1
+                elif teclaAtribuicao == 's':
+                    self.defMagia += 1
+                    pontosDistribuir -= 1
+                elif teclaAtribuicao == 'd':
+                    self.vel += 1
+                    pontosDistribuir -= 1
+                elif teclaAtribuicao == 'f':
+                    self.sorte += 1
+                    pontosDistribuir -= 1
+                elif teclaAtribuicao == 'esc':
+                    print(f'Pontos a distribuir restantes: {pontosDistribuir}')
+                    break
 
     def conversaoAtributos(self):
         hp = self.hp * 10
@@ -72,11 +83,27 @@ class AtributosHeroi:
             'Sorte': sorte
         }
     
-heroi = AtributosHeroi()
-heroi.level += 1
-heroi.distribuicaoPontos()
-atributoHeroi = heroi.conversaoAtributos()
+    def xpUp(self):
+        xp = self.xp
+        xp += 50
 
-print('\nAtributos Heroi:')
-for atributo, valor in atributoHeroi.items():
-    print(f'{atributo}: {valor}')
+        if xp >= 100:
+            xp -= 100
+            self.level += 1
+        
+        return {'Level': self.level, 'XP': self.xp}
+
+    
+while True:
+    heroi = AtributosHeroi()
+    heroi.aumentoNivel()
+    experienciaHeroi = heroi.xpUp()
+    atributoHeroi = heroi.conversaoAtributos()
+
+    print(f'LEVEL: {experienciaHeroi['Level']}')
+    print(f'XP: {experienciaHeroi['XP']}')
+    print('\nAtributos Heroi:')
+    for atributo, valor in atributoHeroi.items():
+        print(f'{atributo}: {valor}')
+
+    input()
